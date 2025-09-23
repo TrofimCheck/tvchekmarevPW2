9e866f98015293ee0e7b43d65bc2b537b85d0710
TrofimCheck <trosha.chekmarev@gmail.com>
2025-09-23 20:05:42 +0300

    Initial Commit

---
+++ tvchekmarevPW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tvchekmarevPW2
+//
+//  Created by Трофим Чекмарев on 23.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
